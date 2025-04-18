79ea043b8c25d1244618555c0bbf0ffc4f062565
Rain-Shi <js6605@columbia.edu>
2025-04-18 23:35:25 +0800

    Update server.py

--- STAT5243_log_server/server.py
+++ STAT5243_log_server/server.py
@@ -30,8 +30,6 @@ def receive_log():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True, port=5000)
 
 @app.route("/status", methods=["GET"])
 def status():
@@ -45,3 +43,5 @@ def status():
     except Exception as e:
         return {"error": str(e)}, 500
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True, port=5000)
