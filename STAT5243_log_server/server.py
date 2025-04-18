651fb27610e3b6117b31b26b7f95c6762b70baf2
Rain-Shi <js6605@columbia.edu>
2025-04-18 22:48:48 +0800

    Update server.py

--- STAT5243_log_server/server.py
+++ STAT5243_log_server/server.py
@@ -31,4 +31,5 @@ def receive_log():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 默认5000，但Render会注入PORT变量
+    app.run(host="0.0.0.0", port=port, debug=True)
